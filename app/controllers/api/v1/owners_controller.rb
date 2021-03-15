e23dd9b0ee5a82c635b458015955046efe11cd4c
Tomoyo Ito <tmyooo73@gmail.com>
2021-03-15 11:40:59 +0900

    fitx

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::OwnersController < ApiController
   def index
+    Rails.logger.info "######### ####owner start"
     about = Owner.first
     ActionController::Base.helpers.sanitize(about.comment)
     render json: about
