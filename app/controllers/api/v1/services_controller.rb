902489c1d439d41f737474a4e36aa691415951c1
Tomoyo Ito <tmyooo73@gmail.com>
2021-03-15 11:38:31 +0900

    fix

---
+++ app/controllers/api/v1/services_controller.rb
@@ -0,0 +1,9 @@
+class Api::V1::ServicesController < ApiController
+  def index
+    Rails.logger.info "######### ####start"
+    @services = Service.all
+    Rails.logger.info "######### #{@services.inspect}"
+    @services = Service.select(:id, :title, :description, :image)
+    Rails.logger.info "######### #{@services.inspect}"
+  end
+end
