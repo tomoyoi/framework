2aa9aca4f747bd1b8d76a6158f9fa25eac9601c8
Tomoyo Ito <tmyooo73@gmail.com>
2021-03-15 09:57:50 +0900

    fix

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -4,20 +4,4 @@ class Api::V1::ServicesController < ApiController
     Rails.logger.info "############# #{services.inspect}"
     render json: services
   end
-
-  def show
-    service = Service.find(id)
-  end
-
-  def create
-    service = Service.find(id)
-  end
-
-  def update
-    service = Service.find(id)
-  end
-
-  def destroy
-    service = Service.find(id)
-  end
 end
